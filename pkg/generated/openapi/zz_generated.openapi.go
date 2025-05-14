15efb506f7a90a38c13f61b24126540b49a283ae
Yuval Kohavi <yuval.kohavi@gmail.com>
2025-05-14 10:01:55 -0400

    add tls knob
    Signed-off-by: Yuval Kohavi <yuval.kohavi@gmail.com>

--- pkg/generated/openapi/zz_generated.openapi.go
+++ pkg/generated/openapi/zz_generated.openapi.go
@@ -1620,6 +1620,15 @@ func schema_kgateway_v2_api_v1alpha1_DynamicForwardProxyBackend(ref common.Refer
 			SchemaProps: spec.SchemaProps{
 				Description: "DynamicForwardProxyBackend is the dynamic forward proxy backend configuration.",
 				Type:        []string{"object"},
+				Properties: map[string]spec.Schema{
+					"tls": {
+						SchemaProps: spec.SchemaProps{
+							Description: "Enabled TLS. When enabled, the backend will be configured to use TLS. System CA will be used for validation. The hostname will be used for SNI and auto SAN validation.",
+							Type:        []string{"boolean"},
+							Format:      "",
+						},
+					},
+				},
 			},
 		},
 	}
