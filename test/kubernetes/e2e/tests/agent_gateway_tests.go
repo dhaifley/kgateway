dce6692b215637cc83b0cae36d714d40f048b97e
David Haifley <david.haifley@solo.io>
2025-09-13 12:14:55 -0400

    feat(deployer): enable zero downtime rollout, graceful shutdown by default
    Signed-off-by: David Haifley <david.haifley@solo.io>

--- test/kubernetes/e2e/tests/agent_gateway_tests.go
+++ test/kubernetes/e2e/tests/agent_gateway_tests.go
@@ -5,6 +5,7 @@ import (
 	"github.com/kgateway-dev/kgateway/v2/test/kubernetes/e2e/features/agentgateway"
 	"github.com/kgateway-dev/kgateway/v2/test/kubernetes/e2e/features/agentgateway/extauth"
 	"github.com/kgateway-dev/kgateway/v2/test/kubernetes/e2e/features/agentgateway/rbac"
+	"github.com/kgateway-dev/kgateway/v2/test/kubernetes/e2e/features/agentgateway/zero_downtime_rollout"
 	"github.com/kgateway-dev/kgateway/v2/test/kubernetes/e2e/features/backendtls"
 	"github.com/kgateway-dev/kgateway/v2/test/kubernetes/e2e/features/local_rate_limit"
 	"github.com/kgateway-dev/kgateway/v2/test/kubernetes/e2e/features/rate_limit"
@@ -18,6 +19,7 @@ func AgentGatewaySuiteRunner() e2e.SuiteRunner {
 	agentGatewaySuiteRunner.Register("GlobalRateLimit", rate_limit.NewAgentGatewayTestingSuite)
 	agentGatewaySuiteRunner.Register("RBAC", rbac.NewTestingSuite)
 	agentGatewaySuiteRunner.Register("BackendTLSPolicy", backendtls.NewAgentGatewayTestingSuite)
+	agentGatewaySuiteRunner.Register("ZeroDowntimeRollout", zero_downtime_rollout.NewTestingSuite)
 
 	return agentGatewaySuiteRunner
 }
